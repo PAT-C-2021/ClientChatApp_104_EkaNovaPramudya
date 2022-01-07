2fd58340919bbb8e05351bbf843228714e362067
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2022-01-07 22:11:01 +0700

    Add project files.

---
+++ ClientChatApp_20190140104_EkaNovaPramudya/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientChatApp_20190140104_EkaNovaPramudya
+{   
+    class Program
+    {
+        public class ClientCallBack : ServiceReference1.IServiceCallBackCallback
+        {            
+            public void pesanKirim(string user, string pesan)
+            {
+                Console.WriteLine("{0}: {1}", user, pesan);
+            }
+        }
+        static void Main(string[] args)
+        {
+            InstanceContext context = new InstanceContext(new ClientCallBack());
+            ServiceReference1.ServiceCallBackClient server = new ServiceReference1.ServiceCallBackClient(context);
+
+            Console.WriteLine("Enter username : ");
+            string nama = Console.ReadLine();
+            server.gabung(nama);
+
+            Console.WriteLine("Kirim Pesan : ");
+            string pesan = Console.ReadLine();
+
+            while (true)
+            {
+                if (!string.IsNullOrEmpty(pesan)) server.kirimPesan(pesan);
+                Console.WriteLine("Kirim Pesan");
+                pesan = Console.ReadLine();
+            }
+        }
+    }
+}
